3204b13649a10563f008432c362bf66fceb99599
Clement Fang <clementfang@Clements-MacBook-Pro.local>
2016-02-27 09:25:23 -0800

    Update forum class example

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,6 +3,8 @@
 # 
 
 import time
+import psycopg2
+import bleach
 
 ## Database connection
 DB = []
@@ -16,8 +18,25 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    ## old code for ref
+    #posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+
+    ## open connnection with psql db and retrieve cursor
+    conn = psycopg2.connect("dbname=forum")
+    curs = conn.cursor();
+    ## insert new  content into specified table/column
+    table = "posts"
+    content_col = "content"
+    time_col = "time"
+    SQL = "SELECT %s,%s FROM %s ORDER BY %s DESC" % (content_col, time_col, table, time_col)
+    curs.execute(SQL);
+    db_data = curs.fetchall()
+
+    ## convert into array of hashes
+    posts = [{'content': str(bleach.clean(str(row[0]))), 'time': str(row[1])} for row in db_data]
+    
+    conn.close()
+
     return posts
 
 ## Add a post to the database.
@@ -27,5 +46,18 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+
+    ## open connnection with psql db and retrieve cursor
+    conn = psycopg2.connect("dbname=forum")
+    curs = conn.cursor();
+    ## insert new  content into specified table/column
+    table = 'posts'
+    col = 'content'
+    SQL = 'INSERT INTO %s (%s) values (%s)' % (table, col, "%s")
+    curs.execute(SQL, (bleach.clean(content),));
+    
+    ## commit change to db
+    conn.commit()
+    conn.close()
+    #t = time.strftime('%c', time.localtime())
+    #DB.append((t, content))
